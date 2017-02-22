03273893049084bb14e9a8275704eb97e76ee621
Hatem DRIRA <hdrira@santech.fr>
2017-02-22 16:45:59 +0100

    version initiale du projet

---
+++ src/main/java/io/ziedhlel/developpersApp/DeveloppersApiApp.java
@@ -0,0 +1,13 @@
+package io.ziedhlel.developpersApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class DeveloppersApiApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloppersApiApp.class, args);
+	}
+}
